d2b46fe8b45e9751753af7d2604eb92ad2995312
Ekaterina <jenadoctora@gmail.com>
2021-09-07 21:04:34 +0500

    commit_hw06

--- Project/src/main/java/MainPage.java
+++ Project/src/main/java/MainPage.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MainPage {
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends BaseView{
+    public NavigationMenu navigationMenu;
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+        navigationMenu = new NavigationMenu(driver);
+    }
 }
